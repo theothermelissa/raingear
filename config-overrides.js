d4f564f83a015907081bdb195429f16d503e7a80
theothermelissa <melissapearlmorgan@gmail.com>
2021-07-23 18:15:34 -0400

    Remove unnecessary Jest and Config

--- config-overrides.js
+++
@@ -1,9 +0,0 @@
-import { override, fixBabelImports } from 'customize-cra';
-
-export default override(
-  fixBabelImports('import', {
-    libraryName: 'antd',
-    libraryDirectory: 'es',
-    style: 'css',
-  }),
-);
