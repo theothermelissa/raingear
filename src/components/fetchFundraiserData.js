64c27f8a9c3413e757ff05880e3d2c5c7dae2975
chisom <chisomekwuribe@gmail.com>
2022-03-16 18:55:49 +0100

    Optimized data fetching

--- src/components/fetchFundraiserData.js
+++ src/components/fetchFundraiserData.js
@@ -32,13 +32,25 @@ export const getFundraisers = async (user, ids, callback) => {
 
     async function createFundraiserList(ids) {
         let fundraisers = [];
-        for (let i = 0; i < ids.length; i++) {
-            let fundraiserData = await getData('Fundraisers', ids[i], 'recordID', fundraiserFields);
-            if (fundraiserData.length) {
+        // for (let i = 0; i < ids.length; i++) {
+        //     let fundraiserData = await getData('Fundraisers', ids[i], 'recordID', fundraiserFields);
+        //     if (fundraiserData.length) {
+        //         fundraisers.push({
+        //             fields: fundraiserData[0].fields,
+        //             id: fundraiserData[0].id,
+        //             role: getRecordType(fundraiserData[0].id, user),
+        //         })
+        //     }
+        // }
+        let fundraiserData = await getData('Fundraisers', ids, 'recordID', fundraiserFields);
+
+        if (fundraiserData.length) {
+
+            for (let i = 0; i < fundraiserData.length; i++) {
                 fundraisers.push({
-                    fields: fundraiserData[0].fields,
-                    id: fundraiserData[0].id,
-                    role: getRecordType(fundraiserData[0].id, user),
+                    fields: fundraiserData[i].fields,
+                    id: fundraiserData[i].id,
+                    role: getRecordType(fundraiserData[i].id, user),
                 })
             }
         }
@@ -92,16 +104,39 @@ export const getFundraisers = async (user, ids, callback) => {
                         }
                     } = guardian;
                     if (sellerIDs) {
-                        for (let k = 0; k < arrayify(sellerIDs).length; k++) {
-                            let sellerData = await getData('Sellers', sellerIDs[k], 'recordID', sellerFields);
-                            const { fields, fields: { Fundraiser } } = sellerData[0];
-                            if (sellerData.length && Fundraiser && id === Fundraiser[0]) {
-                                sellers.push({
-                                    id: sellerData[0]['id'],
-                                    fields: fields,
-                                })
+                        // for (let k = 0; k < arrayify(sellerIDs).length; k++) {
+                        //     let sellerData = await getData('Sellers', sellerIDs[k], 'recordID', sellerFields);
+                        //     const { fields, fields: { Fundraiser } } = sellerData[0];
+                        //     if (sellerData.length && Fundraiser && id === Fundraiser[0]) {
+                        //         sellers.push({
+                        //             id: sellerData[0]['id'],
+                        //             fields: fields,
+                        //         })
+                        //     }
+                        // };
+                        let sellerData = await getData('Sellers', sellerIDs, 'recordID', sellerFields);
+                        // const { fields, fields: { Fundraiser } } = sellerData[0];
+                        // if (sellerData.length && Fundraiser && id === Fundraiser[0]) {
+
+                        //     sellers.push({
+                        //         id: sellerData[0]['id'],
+                        //         fields: fields,
+                        //     })
+                        // }
+                        if (sellerData.length) {
+
+                            for (let i = 0; i < sellerData.length; i++) {
+                                const { fields, fields: { Fundraiser } } = sellerData[i];
+
+                                if (Fundraiser && id === Fundraiser[0]) {
+                                    sellers.push({
+                                        id: sellerData[i]['id'],
+                                        fields: fields,
+                                    })
+                                }
+
                             }
-                        };
+                        }
                     };
                     guardians.push({
                         id: guardianID,
@@ -126,6 +161,8 @@ export const getFundraisers = async (user, ids, callback) => {
 
 
     async function getOrderData(fundraiserList) {
+
+        console.log("Getting orders")
         let fundraisersWithOrders = [];
         for (let i = 0; i < fundraiserList.length; i++) {
             const fundraiser = fundraiserList[i];
@@ -149,6 +186,8 @@ export const getFundraisers = async (user, ids, callback) => {
                             Sellers
                         }
                     } = guardian;
+
+                    console.log({ Sellers })
                     if (Sellers) {
                         for (let k = 0; k < Sellers.length; k++) {
                             let orders = [];
@@ -161,14 +200,25 @@ export const getFundraisers = async (user, ids, callback) => {
                                 }
                             } = seller;
                             if (orderIDs) {
-                                for (let l = 0; l < arrayify(orderIDs).length; l++) {
-                                    let orderData = await getData('Orders', orderIDs[l], 'Order ID', orderFields);
-                                    if (orderData.length) {
+                                // for (let l = 0; l < arrayify(orderIDs).length; l++) {
+                                //     let orderData = await getData('Orders', orderIDs[l], 'Order ID', orderFields);
+                                //     if (orderData.length) {
+                                //         orders.push({
+                                //             id: orderData[0]['id'],
+                                //             fields: orderData[0]['fields']
+                                //         })
+                                //     }
+                                // }
+                                let orderData = await getData('Orders', orderIDs, 'OrderID', orderFields);
+                                if (orderData.length) {
+
+                                    for (let i = 0; i < orderData.length; i++) {
                                         orders.push({
-                                            id: orderData[0]['id'],
-                                            fields: orderData[0]['fields']
+                                            id: orderData[i]['id'],
+                                            fields: orderData[i]['fields']
                                         })
                                     }
+
                                 }
                             }
                             sellers.push({
@@ -218,16 +268,27 @@ export const getFundraisers = async (user, ids, callback) => {
             let guardians = [];
 
             if (sellerGuardians) {
-                for (let j = 0; j < sellerGuardians.length; j++) {
-                    const guardianID = sellerGuardians[j];
-                    let guardianData = await getData('SellerGuardians', guardianID, 'GuardianID', guardianFields)
-                    if (guardianData.length) {
+                // for (let j = 0; j < sellerGuardians.length; j++) {
+                //     const guardianID = sellerGuardians[j];
+                //     let guardianData = await getData('SellerGuardians', guardianID, 'GuardianID', guardianFields)
+                //     if (guardianData.length) {
+                //         guardians.push({
+                //             id: guardianData[0]['id'],
+                //             fields: guardianData[0]['fields']
+                //         })
+                //     }
+                // };
+                let guardianData = await getData('SellerGuardians', sellerGuardians, 'GuardianID', guardianFields)
+                if (guardianData.length) {
+
+                    for (let i = 0; i < guardianData.length; i++) {
                         guardians.push({
-                            id: guardianData[0]['id'],
-                            fields: guardianData[0]['fields']
+                            id: guardianData[i]['id'],
+                            fields: guardianData[i]['fields']
                         })
                     }
-                };
+
+                }
             };
             fundraisersWithGuardians.push({
                 id,
